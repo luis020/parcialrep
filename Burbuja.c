b526e9a2e45cef79c43a32bca6b98423282183b3
luis020 <37189401+luis020@users.noreply.github.com>
2018-10-02 10:22:28 -0600

    Add files via upload

---
+++ Burbuja.c
@@ -0,0 +1,49 @@
+
+
+
+
+int main(int argc, char **argv)
+{
+	int n=0;
+	
+	printf("ingrese un tamanio/n");
+	scanf("%i",&n);
+	
+	int matriz[n][n];
+	
+	for (fila=0; fila<n; fila ++){
+		for (columna=0; columna<n; columna++){
+		
+		printf("ingrese un valor/n");
+		scanf("%i", &matriz[fila][columna]);
+		
+		}
+		
+	}
+	
+	int matriz1[n][n];
+	
+	for (fila1=0; fila1<n; fila1++){
+		for (columna1=0; columna1<n; columna1++){
+		
+		printf("ingrese un valor/n");
+		scanf("%i", &matriz1[fila1][columna1]);
+		
+		}
+		
+	}
+	
+	for (a=0 ; a<b; a++){
+	
+	for (fila=0; fila<n; fila++){
+		
+		for (columna=0; columna<n; columna++){
+			
+			if(matriz[fila][columna]
+	
+	
+	
+	
+	return 0;
+}
+
